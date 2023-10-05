b3cd5aeedee1d20da69564a9b6621bf5b677edaf
GabrieleGrossi <grossigabriele98@gmail.com>
2023-10-05 15:20:49 +0200

    Refactoring and add Hompage

---
+++ src/components/Hompage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>
+                    Multisala Grotta
+                </h1>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scoped">
+    
+</style>
